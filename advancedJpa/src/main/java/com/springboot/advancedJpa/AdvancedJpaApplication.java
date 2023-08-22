a13b02e8f2a74bef8f4645433164164e37db185c
leopard2 <leopard2@KIM-HYEONGBEOM>
2023-08-22 09:26:51 +0900

    1st commit

---
+++ advancedJpa/src/main/java/com/springboot/advancedJpa/AdvancedJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.advancedJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedJpaApplication.class, args);
+	}
+
+}
---
+++ advancedJpa/src/test/java/com/springboot/advancedJpa/AdvancedJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.advancedJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
